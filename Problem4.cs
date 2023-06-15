63f1ae01f064f7b6d5186a8bdc880c166aebc2c9
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-15 10:23:30 +0530

    aaded Prime no problem

---
+++ Problem4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAC
+{
+    internal class Problem4
+    {
+        public static void Primeno()
+        {
+            Console.Write("Enter the value of n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Prime numbers between 2 and {0} are:", n);
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            static bool IsPrime(int number)
+            {
+                if (number < 2)
+                {
+                    return false;
+                }
+
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@
 
             //Console.WriteLine(Problem2.Leap(2021));
 
-            Console.WriteLine(Problem3.IfNumberContains3(12345));
+            //Console.WriteLine(Problem3.IfNumberContains3(12345));
+
+            Problem4.Primeno();
         }
     }
 }
