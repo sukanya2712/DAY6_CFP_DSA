1192dcefaa16ad3379e18b9bb8d5bfa9d9cc6cbc
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-15 10:09:40 +0530

    added leap year problem

--- Problem2.cs
+++ Problem2.cs
@@ -8,5 +8,9 @@ namespace DSAC
 {
     internal class Problem2
     {
+        public static bool Leap(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
     }
 }
