f822d66d3ca8c4a830166b964f7490cb8dce1868
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-15 10:02:47 +0530

    added Problem1

---
+++ Problem1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAC
+{
+    internal class Problem1
+    {
+        public static void swap()
+        {
+            int a = 23;
+            int b = 30;
+
+            int temp;
+
+            Console.WriteLine("Before swapping : ");
+            Console.WriteLine("a is : "+a + " ");
+            Console.WriteLine("b is : "+b);
+
+            temp = b;
+            b = a;
+            a = temp;
+
+            Console.WriteLine("After swapping : ");
+            Console.WriteLine("a is : "+a + " ");
+            Console.WriteLine("b is : "+b);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DSAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1.swap();
+        }
+    }
+}
